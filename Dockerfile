1d1b4f4fca3116da2c05fca37ecbab853691e259
Faisal Halim <ffaisalhalim@gmail.com>
2024-10-02 11:36:27 +0700

    Add .gitignore, Dockerfile, and build.sh

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Gunakan image dasar Python
+FROM python:3.10-slim
+
+# Set working directory di dalam container
+WORKDIR /app
+
+# Salin file requirements.txt ke dalam container
+COPY requirements.txt ./
+
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Salin seluruh kode aplikasi ke dalam container
+COPY . .
+
+# Perintah untuk menjalankan aplikasi menggunakan Gunicorn
+CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:8896", "app:app"]
---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Mendapatkan tanggal hari ini dalam format YYYY-MM-DD
+today=$(date +%Y-%m-%d)
+
+# Nama image
+image_name="gaya-belajar-model:$today"
+
+# Membangun image Docker
+docker build -t $image_name .
+
+# Konfirmasi sebelum menjalankan container
+read -p "Apakah Anda ingin menjalankan container dengan nama 'gaya-belajar-container'? (yes/no): " confirm
+
+if [[ $confirm == "yes" ]]; then
+    # Menjalankan container dengan restart policy
+    docker run -d --restart always -p 8896:8896 --name gaya-belajar-container $image_name
+    echo "Container 'gaya-belajar-container' telah dijalankan dengan restart policy 'always'."
+else
+    echo "Container tidak dijalankan."
+fi
